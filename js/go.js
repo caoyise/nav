9d2c335dd9a971d8d4e5bf092d62865d1ca31b0a
caoyise <caoyise@gmail.com>
2020-09-10 01:07:00 +0800

    new

--- js/go.js
+++
@@ -1,9 +0,0 @@
-function ok() {
-	window.parent.parent.location.href = 'https://caoyise.icu'
-}
-window.setTimeout("ok();", 1000);
-function countDown(secs) {
-	jump.innerText = secs;
-	if (--secs > 0) setTimeout("countDown(" + secs + ")", 1000)
-}
-countDown(3);
